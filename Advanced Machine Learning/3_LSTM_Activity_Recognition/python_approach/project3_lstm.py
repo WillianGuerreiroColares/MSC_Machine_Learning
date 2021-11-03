99b61505eb91f21a1470a3a08e4f5b5e8aa2e958
Willian_Guerreiro_Colares <w.colares@sidi.org.br>
2021-11-03 18:31:21 -0300

    Updated LSTM Code

--- Advanced Machine Learning/3_LSTM_Activity_Recognition/python_approach/project3_lstm.py
+++ Advanced Machine Learning/3_LSTM_Activity_Recognition/python_approach/project3_lstm.py
@@ -2,16 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-
-'''from keras.models import Sequential
+from keras.models import Sequential
+from keras.layers import LSTM
 from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import Dropout
 from keras.layers import LSTM
+
 from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split'''
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
+import seaborn as sns
 
 # summarize scores
 def summarize_results(scores):
@@ -26,79 +29,13 @@ def visualize(data):
     ydata = data[151:302]
     zdata = data[302:]
 
-
     t = np.arange(0, 151, 1)
 
-    fig = plt.figure()
-
-    mag1 = np.sqrt(np.square(list(xdata[0])) + np.square(list(ydata[0])) + np.square(list(zdata[0])))
-    plt.subplot(3,3,1)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Levantando (cadeira)')
-    plt.plot(t,xdata[0],t,ydata[0],t,zdata[0])
-
-    mag2 = np.sqrt(np.square(list(xdata[42])) + np.square(list(ydata[42])) + np.square(list(zdata[42])))
-    plt.subplot(3,3,2)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Levantando (cama)')
-    plt.plot(t,xdata[42],t,ydata[42],t,zdata[42])
-
-    plt.subplot(3,3,3)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Caminhando')
-    plt.plot(t,xdata[88],t,ydata[88],t,zdata[88])
-
-    plt.subplot(3,3,4)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Correndo')
-    plt.plot(t,xdata[461],t,ydata[461],t,zdata[461])
-
-    plt.subplot(3,3,5)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Descendo escada')
-    plt.plot(t,xdata[877],t,ydata[877],t,zdata[877])
-
-    plt.subplot(3,3,6)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Pulando')
-    plt.plot(t,xdata[1135],t,ydata[1135],t,zdata[1135])
-
-    plt.subplot(3,3,7)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Subindo escada')
-    plt.plot(t,xdata[1299],t,ydata[1299],t,zdata[1299])
-
-    plt.subplot(3,3,8)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Deitando na cama')
-    plt.plot(t,xdata[1607],t,ydata[1607],t,zdata[1607])
-
-    plt.subplot(3,3,9)
-    plt.xlabel('Tempo')
-    plt.ylabel('Magnitudes')
-    plt.title('Sentando na cadeira')
-    plt.plot(t,xdata[1669],t,ydata[1669],t,zdata[1669])
-
-    '''axs[1].plot(t,xdata[0])
-    axs[2].plot(t,xdata[0])
-    axs[3].plot(t,xdata[0])
-    axs[4].plot(t,xdata[0])
-    axs[5].plot(t,xdata[0])
-    axs[6].plot(t,xdata[0])
-    axs[7].plot(t,xdata[0])
-    axs[8].plot(t,xdata[0])
-
-    fig.tight_layout()'''
-    #fig.legend(['x ', 'y', 'z'])
-
+    fig, (ax1, ax2, ax3) = plt.subplots(3)
+    fig.suptitle('Vertically stacked subplots')
+    ax1.plot(t, xdata[0], t, ydata[0], t, zdata[0])
+    ax2.plot(t, xdata[1], t, ydata[1], t, zdata[1])
+    ax3.plot(t, xdata[2], t, ydata[2], t, zdata[2])
     plt.show()
 
 def Classes_index(labels, classes):
@@ -112,17 +49,17 @@ def evaluate_model(trainX, trainy, testX, testy):
     verbose, epochs, batch_size = 1, 60, 64
     print(trainX.shape)
     print(trainy.shape)
-    n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+    n_timesteps, n_features, n_outputs = trainX.shape[2], trainX.shape[1], trainy.shape[1]
     model = Sequential()
-    model.add(LSTM(200, input_shape=(n_timesteps,n_features)))
+    model.add(LSTM(250, input_shape=(n_features, n_timesteps)))
     #model.add(Dropout(0.5))
-    model.add(Dense(100, activation='relu'))
+    model.add(Dense(9, activation='relu'))
     model.add(Dense(n_outputs, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     # fit network
     history = model.fit(trainX, trainy, validation_data = (testX, testy), epochs=epochs, batch_size=batch_size, verbose=verbose)
 
-    # list all data in history
+    '''# list all data in history
     print(history.history.keys())
     # summarize history for accuracy
     plt.plot(history.history['accuracy'])
@@ -139,11 +76,11 @@ def evaluate_model(trainX, trainy, testX, testy):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+    plt.show()'''
 
     # evaluate model
     _, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
-    return accuracy
+    return accuracy, model
 
 # fix random seed for reproducibility
 np.random.seed(7)
@@ -151,34 +88,40 @@ np.random.seed(7)
 #Define classes
 classes = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+#Test size
+test_size = 0.85
+
 #Load data and labels
 data = pd.read_excel('dados.xlsx', header=None)
+#print(data, data.shape, type(data))
+#visualize(data)
+
+xdata = np.array(data[:151])
+ydata = np.array(data[151:302])
+zdata = np.array(data[302:])
+
+xdata = np.transpose(xdata)
+ydata = np.transpose(ydata)
+zdata = np.transpose(zdata)
+
+X3D = np.zeros((1724,3,151))
+
+for i in range(1724):
+    mov_block = np.vstack((xdata[i], ydata[i], zdata[i]))
+    X3D[i] = mov_block
+
 labels = pd.read_excel('labels.xlsx', header=None)
 
 #Splitting data according to ratio 0.85
-classes_index = Classes_index(labels, classes)
-
-visualize(data)
-
-print(data, data.shape, type(data))
-
-x = np.array(data)
-print(x)
-x = np.transpose(x)
 
 y = np.array(labels)
 y = np.transpose(y)
 
-'''
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.15, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X3D, y, test_size=0.15)
 
-#print('org',X_train, X_train.shape, len(X_train))
-# reshape input to be [samples, time steps, features]   [151,453,3]
-trainX = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-testX = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-print(trainX, trainX.shape, len(trainX))
-print(testX, testX.shape, len(testX))
+print(X_train, X_train.shape, len(X_train))
+print(X_test, X_test.shape, len(X_test))
 
 analytics_classes = []
 graph_labels = []
@@ -190,12 +133,12 @@ for i in range(9):
 #print(analytics_classes, np.sum(analytics_classes))
 #print(graph_labels)
 
-n = np.arange(1, 10, 1)
+'''n = np.arange(1, 10, 1)
 fig, ax = plt.subplots(1)
 hbars = ax.bar(n, analytics_classes)
 ax.set_xticks(classes)
 ax.bar_label(hbars, labels = graph_labels)
-plt.show()
+plt.show()'''
 
 #Preprocessing and encoding
 y_train = y_train - 1
@@ -208,10 +151,43 @@ y_test = to_categorical(y_test)
 
 # run an experiment
 scores = list()
+model_list = list()
 for r in range(10):
-    score = evaluate_model(trainX, y_train, testX, y_test)
+    score, model = evaluate_model(X_train, y_train, X_test, y_test)
+    model_list.append(model)
     score = score * 100.0
     print('>#%d: %.3f' % (r+1, score))
     scores.append(score)
 # summarize results
-summarize_results(scores)'''
+summarize_results(scores)
+
+
+best_model_index = np.argmax(scores)
+print('Better model: ', best_model_index + 1)
+
+best_model = model_list[best_model_index]
+
+predicted = best_model.predict(X_test)
+
+P = np.round(predicted)
+
+CM = confusion_matrix(y_test.argmax(axis=1), P.argmax(axis=1))
+
+print(CM)
+
+con_mat_norm = np.around(CM.astype('float') / CM.sum(axis=1)[:, np.newaxis], decimals=2)
+
+
+con_mat_df = pd.DataFrame(CM,
+                     index = classes,
+                     columns = classes)
+
+figure = plt.figure(figsize=(8, 8))
+sns.heatmap(con_mat_df, annot=True,cmap=plt.cm.Blues)
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+best_model.save("model.h5")
+
